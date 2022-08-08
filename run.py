d5540a785e61656c3422b6d4629f344a2f141e12
GriffinSar <caladoniasaz@gmail.com>
2022-08-08 20:32:58 +0000

    add some code

--- run.py
+++ run.py
@@ -11,9 +11,37 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Sales_Program')
-
 price = SHEET.worksheet('Price')
 
-data = price.get_all_values()
+select = input("Please select a calculator mode, Type renewal or currency: ")
+product = input("Please select your product, Toad or Kace: ")
+level = input("Please select support level, standard, mid, premier: ")
+pricuplift = int(input("Please enter the price to uplift: "))e_
+print(f"You have chosen {select} for {product} support level {level} and last years price was {price_uplift}.")
 
-print(data)
+
+
+#this is how you get access to one individual cell
+data = int(price.acell( 'B2').value)
+
+def uplift_cal():
+    if price_uplift == data:
+        print("No Uplift List Price has Been Reached")
+    else: 
+        final = price_uplift * 1.3
+        print(final)
+
+def mode_select(x):
+    if select == "renewal":
+        uplift_cal()
+    elif select == "currency":
+        print("not created yet")
+
+
+mode_select(select)
+
+
+
+#Create a function to uplift a price entered by a user
+
+uplift_cal()
