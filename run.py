386a5610af7fd129edc8517e4031d9815e26f7f2
GriffinSar <caladoniasaz@gmail.com>
2022-08-14 15:54:42 +0000

    Add function

--- run.py
+++ run.py
@@ -23,6 +23,66 @@ SHEET = GSPREAD_CLIENT.open('Sales_Program')
 price = SHEET.worksheet('Price')
 stored_info = SHEET.worksheet('database')
 
+data_standard_toad = float(price.acell('B2').value)
+data_mid_toad = float(price.acell('B3').value)
+data_prem_toad = float(price.acell('B4').value)
+
+def toad_stan():
+    """Function to price standard Toad Quotes"""
+
+    if quote < data_standard_toad:
+        cost = quote * 1.03
+        print(Fore.CYAN + Style.BRIGHT + f"Your uplifted price is {cost}")
+    elif quote >= data_standard_toad:
+        print(Fore.CYAN + Style.BRIGHT + f"Your quote has reached the list \
+        price of {data_standard_toad} no uplift needed.")
+
+    print("Would you like pricing for the second and third year? type Y/N")
+    multi_year = input("Y/N: ")
+    second_year = cost /100 * 90 
+    print(second_year)
+    third_year = cost /100 * 85
+
+    if multi_year == "Y":
+        print(Fore.CYAN + Style.BRIGHT + f"Second year price {second_year}.")
+        print(Fore.CYAN + Style.BRIGHT + f"Third year price {third_year}.")
+    else :
+        print(Fore.CYAN + Style.BRIGHT + "nothing")
+
+
+def toad_mid():
+    """Function to price mid Toad Quotes"""
+
+    if quote < data_mid_toad:
+        cost = quote * 1.05
+        print(f"Your uplifted price is {cost}")
+    elif quote >= data_mid_toad:
+        print(f"Your quote has reached the list \
+        price of {data_mid_toad} no uplift needed.")
+
+def toad_prem():
+    """Function to price premiere Toad Quotes"""
+
+    if quote < data_prem_toad:
+        cost = quote * 1.07
+        print(Fore.CYAN + Style.BRIGHT + f"Your uplifted price is {cost}")
+    elif quote >= data_prem_toad:
+        print(Fore.CYAN + Style.BRIGHT + f"Your quote has reached the list \
+        price of {data_prem_toad} no uplift needed.")
+
+def toad_pricing():
+    """function to uplift the price"""
+    level = str(input("Standard,Mid,Premiere: "))
+
+    if level == "Standard":
+        toad_stan()
+    elif level == "Mid":
+        toad_mid()
+    elif level == "Premiere":
+        toad_prem()
+    else:
+        print(f"{level} is not a valid input try again")
+        toad_pricing()
 
 def new_customer():
     print("To get started, please enter your username.")
@@ -33,11 +93,30 @@ def new_customer():
     cust_name = input("Enter your customer name here:\n")
 
     if cust_name.isalpha() and len(cust_name) > 1 and len(cust_name) < 16:
-        welcome_user()
+        print("Customer name accepted")
     else:
-         print(Fore.LIGHTYELLOW_EX +
-                  "\nThe username you have entered is not valid, \
-please try again.\n")
+        print(Fore.LIGHTYELLOW_EX +
+                "\nThe username you have entered is not valid, \
+                please try again.\n")
+
+    print(Fore.CYAN + Style.BRIGHT +
+    "Please enter Toad if your Quote is for a Toad \
+    \nproduct.")
+    print(Fore.CYAN + Style.BRIGHT +
+    "Please enter Kace if your Quote is for a \
+    \nKace Product.\n")
+    
+    global type
+    type = input("Please enter your choice here:\n")
+    global quote
+    quote = float(input("Amount: "))
+
+    if type == "Toad":
+        toad_pricing()   
+    elif type == "Kace":
+        print("Goodbye")      
+    else:
+        print(Fore.LIGHTYELLOW_EX + "Invalid input, please try again.\n")
 
 def hist_data():
     """
@@ -68,7 +147,7 @@ def hist_data():
                 break
             elif selection == "c":
                 print(f"{Fore.LIGHTMAGENTA_EX}{Style.BRIGHT}\n\
-Thank you for using the calculator and goodbye.")
+                Thank you for using the calculator and goodbye.")
                 break
             else:
                 print(Fore.LIGHTYELLOW_EX +
@@ -80,7 +159,7 @@ Thank you for using the calculator and goodbye.")
         print(Fore.LIGHTYELLOW_EX + "Returning to the main menu...")
         first_page()
 
-        
+
 def first_page():
     """ 
         Intro Page
