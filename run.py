3ffb07e8d44624e04288001307291aefa9418764
GriffinSar <caladoniasaz@gmail.com>
2022-08-14 15:28:13 +0000

    Ass function for historical data

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ from google.oauth2.service_account import Credentials
 import colorama
 from colorama import Fore, Style
 colorama.init(autoreset=True)
+import pandas as pd
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -20,77 +21,41 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Sales_Program')
 price = SHEET.worksheet('Price')
-
-""" Variables for pricing"""
-
-data_standard_toad = float(price.acell('B2').value)
-data_mid_toad = float(price.acell('B3').value)
-data_prem_toad = float(price.acell('B4').value)
+stored_info = SHEET.worksheet('database')
 
 
-def toad_stan():
-    """Function to price standard Toad Quotes"""
+def new_customer():
+    print("To get started, please enter your username.")
+    print("Usernames must be between 2 and 15 characters,")
+    print("and should contain only letters from a to z.\n")
+    
+    global cust_name
+    cust_name = input("Enter your customer name here:\n")
 
-    if quote < data_standard_toad:
-        cost = quote * 1.03
-        print(Fore.CYAN + Style.BRIGHT + f"Your uplifted price is {cost}")
-    elif quote >= data_standard_toad:
-        print(Fore.CYAN + Style.BRIGHT + f"Your quote has reached the list \
-        price of {data_standard_toad} no uplift needed.")
-
-    print("Would you like pricing for the second and third year? type Y/N")
-    multi_year = input("Y/N: ")
-    second_year = cost /100 * 90 
-    print(second_year)
-    third_year = cost /100 * 85
-
-    if multi_year == "Y":
-        print(Fore.CYAN + Style.BRIGHT + f"Second year price {second_year}.")
-        print(Fore.CYAN + Style.BRIGHT + f"Third year price {third_year}.")
-    else :
-        print(Fore.CYAN + Style.BRIGHT + "nothing")
-
-
-def toad_mid():
-    """Function to price mid Toad Quotes"""
-
-    if quote < data_mid_toad:
-        cost = quote * 1.05
-        print(f"Your uplifted price is {cost}")
-    elif quote >= data_mid_toad:
-        print(f"Your quote has reached the list \
-        price of {data_mid_toad} no uplift needed.")
-
-def toad_prem():
-    """Function to price premiere Toad Quotes"""
-
-    if quote < data_prem_toad:
-        cost = quote * 1.07
-        print(Fore.CYAN + Style.BRIGHT + f"Your uplifted price is {cost}")
-    elif quote >= data_prem_toad:
-        print(Fore.CYAN + Style.BRIGHT + f"Your quote has reached the list \
-        price of {data_prem_toad} no uplift needed.")
-
-def toad_pricing():
-    """function to uplift the price"""
-    level = str(input("Standard,Mid,Premiere: "))
-
-    if level == "Standard":
-        toad_stan()
-    elif level == "Mid":
-        toad_mid()
-    elif level == "Premiere":
-        toad_prem()
+    if cust_name.isalpha() and len(cust_name) > 1 and len(cust_name) < 16:
+        welcome_user()
     else:
-        print(f"{level} is not a valid input try again")
-        toad_pricing()
+         print(Fore.LIGHTYELLOW_EX +
+                  "\nThe username you have entered is not valid, \
+please try again.\n")
 
-
-def start_page():
-    """ 
-    Introduction page for user
+def hist_data():
     """
+    Allows user to view saved details
+    """
+    cust_name = input("Enter your customer name here:\n")
 
+    if stored_info.find(cust_name, in_column=1):
+         print(Fore.LIGHTGREEN_EX + Style.BRIGHT +
+              "\nThe details you currently have saved are:\n")
+         df = pd.DataFrame(stored_info.get_all_records())
+         user_record = df.loc[df['cust_name'] == cust_name].to_string(index=False)
+         print(f"{Fore.LIGHTCYAN_EX }{Style.BRIGHT}\n{user_record}\n")
+
+def first_page():
+    """ 
+        Intro Page
+    """
     print(Fore.CYAN + Style.BRIGHT +
         "Welcome to your Renewal Calculator!\n")
     print(Fore.MAGENTA + Style.BRIGHT + """\
@@ -109,26 +74,18 @@ def start_page():
     print("renewal cost and get this years uplifted price.")
     print("Along with multi-year pricing.\n")
 
-
     print(Fore.CYAN + Style.BRIGHT +
-    "Please enter Toad if your Quote is for a Toad \
-    \nproduct.")
+    "To start enter 1 if you want to start a new calculation.")
     print(Fore.CYAN + Style.BRIGHT +
-    "Please enter Kace if your Quote is for a \
-    \nKace Product.\n")
+    "Enter 2 if you want to pull historical data for a customer")
 
-    type = input("Please enter your choice here:\n")
-    global quote
-    quote = float(input("Amount: "))
-    global customer
-    customer = input("Input your customer name: ")
-
-    if type == "Toad":
-        toad_pricing()   
-    elif type == "Kace":
-        print("Goodbye")      
+    mode = input("Please enter your choice here:\n")
+    
+    if mode == "1":
+        new_customer()
+    elif mode == "2":
+        hist_data()
     else:
         print(Fore.LIGHTYELLOW_EX + "Invalid input, please try again.\n")
 
-
-start_page()
+first_page()
